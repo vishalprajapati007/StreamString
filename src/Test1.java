c3f80c2635194c9b61eacfcd86ca78bfcf02865a
Visii007it <vishal143.vp72@gmail.comg>
2023-04-18 13:34:09 +0530

    new file

--- src/Test1.java
+++ src/Test1.java
@@ -9,117 +9,96 @@ import org.testng.annotations.Test;
 
 public class Test1 {
    // @Test
-	public void Demo() {
-		// Count of name Start With A
-	ArrayList<String> name = new ArrayList<String>();
-	name.add("Abhi");
-	name.add("Adity");
-	name.add("demo");
-	name.add("visii");
-	name.add("Anmol");
-	int count = 0;
+    public void Demo() {
+        // Count of name Start With A
+    ArrayList<String> name = new ArrayList<String>();
+    name.add("Abhi");
+    name.add("Adity");
+    name.add("demo");
+    name.add("visii");
+    name.add("Anmol");
+    int count = 0;
 
-	for(int i=0; i<name.size(); i++)
-	{
-	 String s= name.get(i);
-	 if(s.startsWith("A"))
-	 {
-	     count++;
-	 }
-	}
-	 System.out.println(count);
-	}
-    
-<<<<<<< HEAD
+for(int i=0; i<name.size(); i++)
+{
+ String s= name.get(i);
+ if(s.startsWith("A"))
+ {
+     count++;
+ }
+}
+ System.out.println(count);
+}
 
-    //@Test	
-=======
-    @Test	
->>>>>>> a189ac8dc85a2f99adcf8d88ebac8789cd038316
-    public void streamfilter() {
-    	
-    	ArrayList<String> name = new ArrayList<String>();
-    	name.add("Abhi");
-    	name.add("Adity");
-    	name.add("demo");
-    	name.add("visii");
-    	name.add("Anmol");
-<<<<<<< HEAD
-    	
-    	// for find name start with A
-    	Long V=  name.stream().filter(s -> s.startsWith("A")).count();
-    	System.out.println(V);
-    	
-    	
-        long D= Stream.of("Abhi","Adity","Don","Visii","Anmol").filter(s->
-=======
-    	// count of name start with A
-    	Long V=  name.stream().filter(s -> s.startsWith("A")).count();
-    	System.out.println(V);
-    	
-        long D= Stream.of("Abhi","Adity","demo","visii","Anmol").filter(s->
->>>>>>> a189ac8dc85a2f99adcf8d88ebac8789cd038316
-       {
-    	   s.startsWith("A");
-    	   return true;
-       }).count();
-        
-       System.out.println(D);
-<<<<<<< HEAD
-       
-       // print all from arraylist 
-=======
-       //print all the name in arraylist
->>>>>>> a189ac8dc85a2f99adcf8d88ebac8789cd038316
-       name.stream().filter(s-> s.length()>4).limit(2).forEach(s->System.out.println(s));
-    	
-       }
+@Test    
+public void streamfilter() {
     
+    ArrayList<String> name = new ArrayList<String>();
+    name.add("Abhi");
+    name.add("Adity");
+    name.add("demo");
+    name.add("visii");
+    name.add("Anmol");
+    // count of name start with A
+    Long V=  name.stream().filter(s -> s.startsWith("A")).count();
+    System.out.println(V);
+    
+    long D= Stream.of("Abhi","Adity","demo","visii","Anmol").filter(s->
+   {
+       s.startsWith("A");
+       return true;
+   }).count();
+    
+   System.out.println(D);
+   //print all the name in arraylist
+   name.stream().filter(s-> s.length()>4).limit(2).forEach(s->System.out.println(s));
+    
+   }
    // @Test
     public void stringMap()
     {
-    	ArrayList<String> name = new ArrayList<String>();
-    	name.add("Don");
-    	name.add("Rekha");
-    	name.add("Sanket");
-    	
-    	//print last letter "a" is.
-    	Stream.of("Abhi","Ahemad","Bkhshay","Rama","Nobita").filter(s->s.endsWith("a")).map(s->s.toUpperCase()).forEach(s->System.out.println(s));
-    	// print letter in uppercase
-    	
+        ArrayList<String> name = new ArrayList<String>();
+        name.add("Don");
+        name.add("Rekha");
+        name.add("Sanket");
+
+        //print last letter "a" is.
+        Stream.of("Abhi","Ahemad","Bkhshay","Rama","Nobita").filter(s->s.endsWith("a")).map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+        // print letter in uppercase
+
          List<String> abc = Arrays.asList("Abhi","Ahemad","Bkhshay","Rama","Nobita");
-         
+
          abc.stream().filter(s->s.startsWith("B")).sorted().map(s->s.toUpperCase()).forEach(s->System.out.println(s));
-         
+
          //marging 2 list
-         
+
          Stream<String> xyz = Stream.concat(name.stream(), abc.stream());
         // xyz.sorted().forEach(s->System.out.println(s));
-    	boolean flag= xyz.anyMatch(s->s.equalsIgnoreCase("Rekha"));
-    	System.out.println(flag);
-    	Assert.assertTrue(flag);
+        boolean flag= xyz.anyMatch(s->s.equalsIgnoreCase("Rekha"));
+        System.out.println(flag);
+        Assert.assertTrue(flag);
     }
-    
+
   //  @Test
     public void stramColllect()
     {
-    	
-        List<String>ls=	Stream.of("Aman","Ankit","Amit","Vishal","Deep").filter(s->s.startsWith("A")).map(s->s.toUpperCase())
-    	.collect(Collectors.toList());
-    	System.out.println(ls.get(2));
-    	
-    	//interview exp
-    	
-    	//print uniq number from array = 1,2,2,3,5,5,9
-    	// short the array = 2nd print
-    	
-    	List<Integer> value= Arrays.asList(1,2,2,3,5,5,9);
-    	//value.stream().distinct().forEach(s->System.out.println(s));
-    	
-    	List<Integer>vp= value.stream().distinct().sorted().collect(Collectors.toList());
-    	System.out.println(vp.get(1));
-    	
-    	
-    	
+
+        List<String>ls=    Stream.of("Aman","Ankit","Amit","Vishal","Deep").filter(s->s.startsWith("A")).map(s->s.toUpperCase())
+        .collect(Collectors.toList());
+        System.out.println(ls.get(2));
+
+        //interview exp
+
+        //print uniq number from array = 1,2,2,3,5,5,9
+        // short the array = 2nd print
+
+        List<Integer> value= Arrays.asList(1,2,2,3,5,5,9);
+        //value.stream().distinct().forEach(s->System.out.println(s));
+
+        List<Integer>vp= value.stream().distinct().sorted().collect(Collectors.toList());
+        System.out.println(vp.get(1));
+
+
+
     }
 }
